ba02b017afb46db7e7ee126b63431c11d55aeee0
laukev7 <laukev7@gmail.com>
2013-12-17 08:20:58 -0500

    Changed rule to !fox command

---
+++ fox_why.py
@@ -0,0 +1,5 @@
+from willie.module import commands
+
+@commands('fox')
+def fox_why(bot, trigger):
+    bot.reply("fox_/fox/goldmember was banned for inappropiate behavior in the past from having 5 prior bans. He was spreading rumors, couldn\'t back his sources, was disinfo and had rude behavior.")
