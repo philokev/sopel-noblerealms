2b30fe1f8db0c21d9be45ca23a515fc3f2a8c6b6
laukev7 <laukev7@gmail.com>
2013-12-19 15:48:06 -0500

    Corrected channel bug

--- autovoice.py
+++ autovoice.py
@@ -3,4 +3,6 @@ from willie.module import rule, event
 @event('JOIN')
 @rule(r'.*')
 def autovoice(bot, trigger):
-    bot.write(['MODE', channel, "+v", trigger.nick])
+    channel = trigger.sender
+    nick = trigger.nick
+    bot.write(['MODE', channel, "+v", nick])
